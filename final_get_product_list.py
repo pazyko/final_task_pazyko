d56b73504bbe505f48a51a5373e0fc2ad45492c7
Svetlana <32611278+pazyko@users.noreply.github.com>
2019-04-14 14:49:35 +0300

    Delete final_get_product_list.py

--- final_get_product_list.py
+++
@@ -1,9 +0,0 @@
-class GetProductList(object):
-
-    def __init__(self, rowid, name, price):
-        self.rowid = rowid
-        self.name = name
-        self.price = price
-
-    def get_tuple_to_product(self):
-        return self.rowid, self.name, self.price
